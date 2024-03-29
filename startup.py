2414662222954980ee4e7907dbc98b2c0889e298
William <william.jsson@hotmail.com>
2024-03-29 15:15:05 +0100

    Cleanup

--- startup.py
+++ startup.py
@@ -1,66 +1,97 @@
 import os
 import subprocess
 import sys
+
 # http://host.docker.internal:7860/
 
 name_of_docker_container = "open-webui"
 directory_to_stable_diffusion_webui_absolute = "C://DEV//AI_GEN_THING//stable-diffusion-webui//"
 
+starting = '''
+                                                                                       
+   _|_|_|    _|                            _|      _|                                  
+ _|        _|_|_|_|    _|_|_|  _|  _|_|  _|_|_|_|      _|_|_|      _|_|_|              
+   _|_|      _|      _|    _|  _|_|        _|      _|  _|    _|  _|    _|              
+       _|    _|      _|    _|  _|          _|      _|  _|    _|  _|    _|              
+ _|_|_|        _|_|    _|_|_|  _|            _|_|  _|  _|    _|    _|_|_|  _|  _|  _|  
+                                                                       _|              
+                                                                   _|_|                
+'''
+
+stopping = '''
+                                                                                       
+   _|_|_|    _|                                    _|                                  
+ _|        _|_|_|_|    _|_|    _|_|_|    _|_|_|        _|_|_|      _|_|_|              
+   _|_|      _|      _|    _|  _|    _|  _|    _|  _|  _|    _|  _|    _|              
+       _|    _|      _|    _|  _|    _|  _|    _|  _|  _|    _|  _|    _|              
+ _|_|_|        _|_|    _|_|    _|_|_|    _|_|_|    _|  _|    _|    _|_|_|  _|  _|  _|  
+                               _|        _|                            _|              
+                               _|        _|                        _|_|                
+'''
+
+startup_script = """
+                                                                                                                                 
+   _|_|_|    _|                            _|                                _|_|_|                      _|              _|      
+ _|        _|_|_|_|    _|_|_|  _|  _|_|  _|_|_|_|  _|    _|  _|_|_|        _|          _|_|_|  _|  _|_|      _|_|_|    _|_|_|_|  
+   _|_|      _|      _|    _|  _|_|        _|      _|    _|  _|    _|        _|_|    _|        _|_|      _|  _|    _|    _|      
+       _|    _|      _|    _|  _|          _|      _|    _|  _|    _|            _|  _|        _|        _|  _|    _|    _|      
+ _|_|_|        _|_|    _|_|_|  _|            _|_|    _|_|_|  _|_|_|        _|_|_|      _|_|_|  _|        _|  _|_|_|        _|_|  
+                                                             _|                                              _|                  
+                                                             _|                                              _|                 
+"""
 
 def start_all():
-    print("Starting up...")
-    # ############### DOCKER OPEN WEBUI ###############
-    print("Starting up Open WebUI docker container...")
-    command = ["docker", "start", name_of_docker_container]
-    subprocess.check_call(command, shell=False)
+    print(starting)
+    try:
+        # ############### DOCKER OPEN WEBUI ###############
+        print("Starting up Open WebUI docker container...")
+        command = ["docker", "start", name_of_docker_container]
+        subprocess.check_call(command, shell=False)
 
-    # #################################################
+        # ########## STABLE DIFFUSION WEBUI API ###########
+        print("Starting up Stable Diffusion WebUI API...")
+        os.chdir(directory_to_stable_diffusion_webui_absolute)
+        command = ["start", "cmd", "/c", "webui.bat --api --listen"]
+        subprocess.Popen(command, shell=True)
 
-    # ########## STABLE DIFFUSION WEBUI API ###########
-    print("Starting up Stable Diffusion WebUI API...")
-    os.chdir(directory_to_stable_diffusion_webui_absolute)
-    command = ["start", "cmd", "/c", "webui.bat --api --listen"]
-    subprocess.Popen(command, shell=True)
+        # ############### OLLAMA SERVE ####################
+        print("Starting up Ollama Serve...")
+        command = ["start", "wsl", "ollama", "serve"]
+        subprocess.Popen(command, shell=True)
+    except Exception as e:
+        print("Error starting up: ", e)
 
-    # #################################################
-
-    # ############### OLLAMA SERVE ####################
-    print("Starting up Ollama Serve...")
-    command = ["wsl", "ollama", "serve"]
-    subprocess.Popen(command, shell=True)
-
+    print("[ All services started successfully. ]")
 
 def stop_all():
-    try:
-        print("Shutting down...")
-
-        # List all container
-        os.system("docker ps -a")
-
+    print(stopping)
         # ############### DOCKER OPEN WEBUI ###############
+    try:
         print("Stopping Open WebUI docker container...")
         command = ["docker", "stop", name_of_docker_container]
         subprocess.check_call(command, shell=False)
-
-        # #################################################
+    except Exception as e:
+        print("Error stopping Open WebUI docker container: ", e)
 
         # ########## STABLE DIFFUSION WEBUI API ###########
+    try:
         print("Stopping Stable Diffusion WebUI API...")
         os.chdir(directory_to_stable_diffusion_webui_absolute)
         command = ["taskkill", "/IM", "python.exe", "/F"]
         subprocess.check_call(command, shell=False)
-
-        # #################################################
+    except Exception as e:
+        print("Error stopping Stable Diffusion WebUI API: ", e)
 
         # ############### OLLAMA SERVE ####################
+    try:
         print("Stopping Ollama Serve...")
-        command = ["taskkill", "/IM", "/F"]
-        subprocess.check_call(command, shell=False)
+        command = ["wsl", "killall", "ollama"]
+        subprocess.run(command, check=True)
+        print("Ollama Serve stopped successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error stopping Ollama Serve: {e}")
 
-        # #################################################
-    except Exception as e:
-        print("Error: ", e)
-
+    print("[ All services stopped successfully. ]")
 
 def main():
     if start:
@@ -71,14 +102,16 @@ def main():
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("Invalid argument\nUsage: python startup.py --start/--stop")
+        print(startup_script)
+        print("Invalid argument\nUsage: python3 startup.py --start/--stop")
         exit(1)
     if sys.argv[1] == "--start":
         start = True
     elif sys.argv[1] == "--stop":
         start = False
     else:
-        print("Invalid argument\nUsage: python startup.py --start/--stop")
+        print(startup_script)
+        print("Invalid argument\nUsage: python3 startup.py --start/--stop")
         exit(1)
 
     main()
