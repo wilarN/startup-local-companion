30ae78b25fd5050938c6dd962cfcd09e615d99ee
William <william.jsson@hotmail.com>
2024-08-02 11:30:54 +0200

    Update

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ Automation script to startup and stop in windows (ollama in wsl):
 
 
 In Open webui --> `http://host.docker.internal:7860/` is needed in the AUTOMATIC1111 Base URL in the Image Settings if it's being used this way...
+Also if you're using Openedai-speech in docker, make sure you set the api address to `http://localhost:8000/v1` and the api key to `sk-111111111`
+[text](https://github.com/matatonic/openedai-speech)
 
 ![image](https://github.com/wilarN/startup-local-companion/assets/68875195/c1bb34ed-7c48-4711-a399-e8283e1ac273)
 ![image](https://github.com/wilarN/startup-local-companion/assets/68875195/8f4b7af2-ddce-4bb9-8fd9-252ac9c88158)
--- startup.py
+++ startup.py
@@ -7,7 +7,7 @@ import sys
 name_of_docker_container = "open-webui"
 
 # Change this to your local path to the stable-diffusion-webui directory. This script uses the batch file to start the API. (webui.bat --api --listen)
-directory_to_stable_diffusion_webui_absolute = "C://example//path//to//stable-diffusion-webui" # Change this to your path
+directory_to_stable_diffusion_webui_absolute = ""
 
 starting = '''
                                                                                        
@@ -42,6 +42,7 @@ startup_script = """
                                                              _|                                              _|                 
 """
 
+
 def start_all():
     print(starting)
     try:
@@ -56,46 +57,80 @@ def start_all():
         command = ["start", "cmd", "/c", "webui.bat --api --listen"]
         subprocess.Popen(command, shell=True)
 
-        # ############### OLLAMA SERVE ####################
-        print("Starting up Ollama Serve...")
-        command = ["start", "wsl", "ollama", "serve"]
-        subprocess.Popen(command, shell=True)
+        # ############### DOCKER OPEN WEBUI ###############
+        print("Starting up Ollama docker container...")
+        command = ["docker", "start", "ollama"]
+        subprocess.check_call(command, shell=False)
+
+        # ############### DOCKER OPENED AI API ###############
+        print("Starting up Openedai-speech docker container...")
+        command = ["docker", "start", "openedai-speech"]
+        subprocess.check_call(command, shell=False)
+
+        # # ############### OLLAMA SERVE ####################
+        # print("Starting up Ollama Serve...")
+        # command = ["start", "wsl", "ollama", "serve"]
+        # subprocess.Popen(command, shell=True)
     except Exception as e:
         print("Error starting up: ", e)
+        return False
 
     print("[ All services started successfully. ]")
     print("Dont forget to add http://host.docker.internal:7860/ to the Open WebUI's image gen API.")
     print("Open the webgui: http://localhost:3000/")
+    return True
 
 def stop_all():
     print(stopping)
+    try:
         # ############### DOCKER OPEN WEBUI ###############
-    try:
-        print("Stopping Open WebUI docker container...")
-        command = ["docker", "stop", name_of_docker_container]
-        subprocess.check_call(command, shell=False)
+        try:
+            print("Stopping Open WebUI docker container...")
+            command = ["docker", "stop", name_of_docker_container]
+            subprocess.check_call(command, shell=False)
+        except Exception as e:
+            print("Error stopping Open WebUI docker container: ", e)
+
+            # ########## STABLE DIFFUSION WEBUI API ###########
+        try:
+            print("Stopping Stable Diffusion WebUI API...")
+            os.chdir(directory_to_stable_diffusion_webui_absolute)
+            command = ["taskkill", "/IM", "python.exe", "/F"]
+            subprocess.check_call(command, shell=False)
+        except Exception as e:
+            print("Error stopping Stable Diffusion WebUI API: ", e)
+
+            # ############### OLLAMA SERVE ####################
+
+        try:
+            print("Stopping Openedai-speech docker container...")
+            command = ["docker", "stop", "openedai-speech"]
+            subprocess.check_call(command, shell=False)
+        except Exception as e:
+            print("Error stopping Openedai-speech docker container: ", e)
+
+        # ############### DOCKER OPEN WEBUI ###############
+        try:
+            print("Stopping Ollama docker container...")
+            command = ["docker", "stop", "ollama"]
+            subprocess.check_call(command, shell=False)
+        except Exception as e:
+            print("Error stopping Ollama docker container: ", e)
+
+        # try:
+        #     print("Stopping Ollama Serve...")
+        #     command = ["wsl", "killall", "ollama"]
+        #     subprocess.run(command, check=True)
+        #     print("Ollama Serve stopped successfully.")
+        # except subprocess.CalledProcessError as e:
+        #     print(f"Error stopping Ollama Serve: {e}")
+
     except Exception as e:
-        print("Error stopping Open WebUI docker container: ", e)
+        print("Error stopping: ", e)
+        return False
+    return True
 
-        # ########## STABLE DIFFUSION WEBUI API ###########
-    try:
-        print("Stopping Stable Diffusion WebUI API...")
-        os.chdir(directory_to_stable_diffusion_webui_absolute)
-        command = ["taskkill", "/IM", "python.exe", "/F"]
-        subprocess.check_call(command, shell=False)
-    except Exception as e:
-        print("Error stopping Stable Diffusion WebUI API: ", e)
 
-        # ############### OLLAMA SERVE ####################
-    try:
-        print("Stopping Ollama Serve...")
-        command = ["wsl", "killall", "ollama"]
-        subprocess.run(command, check=True)
-        print("Ollama Serve stopped successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error stopping Ollama Serve: {e}")
-
-    print("[ All services stopped successfully. ]")
 
 def main():
     if start:
